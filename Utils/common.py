6232c773007b3a459f47bcde292a6cdf843af8c3
voitseh <voitseh.kaminsky@gmail.com>
2017-10-25 23:30:12 +0300

    Create Utils dir with common.py file

---
+++ Utils/common.py
@@ -0,0 +1,17 @@
+import os, sys, glob
+import shutil
+
+
+def copy(subfolder, dir_path, names=None):
+    index = 0
+    for filename in glob.glob(os.path.join( dir_path + subfolder, "*.*")):
+        index += 1
+        shutil.copy(filename,  dir_path)
+    if os.path.exists(dir_path+subfolder):
+        shutil.rmtree( dir_path + subfolder)
+            
+    
+def png_to_jpg_converter(filename, distination_path, img_name):
+    im = Image.open(filename)
+    im.save(distination_path + img_name.split(".png")[0] + ".jpg","jpeg")
+    os.remove(filename)
