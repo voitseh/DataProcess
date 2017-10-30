0533b9c99a410a3094b6b8e2c2f8462a2f82f804
voitseh <voitseh.kaminsky@gmail.com>
2017-10-30 16:51:45 +0200

    Update common.py

--- Utils/common.py
+++ Utils/common.py
@@ -4,6 +4,7 @@ from PIL import Image
 
 def copy_files(from_dir_path, destination_dir_path):
     if os.path.exists(from_dir_path):
+        # TODO del if
         if not os.path.exists(destination_dir_path):  make_directory(destination_dir_path)
         for filename in glob.glob(os.path.join( from_dir_path, "*.*")):
             copy_file(filename, destination_dir_path)
@@ -20,16 +21,15 @@ def remove_directory(directory_path):
     if os.path.isdir(directory_path):
         shutil.rmtree(directory_path)
     else: 
-         print(directory_path + " is already deleted!")
+        #TODO: "{0}.format()
+        print(directory_path + " is already deleted!")
 
 def remove_directories(dir_list):
     if type(dir_list) == list:
         for i in range(len(dir_list)):
-            if os.path.isdir(dir_list[i]):
-                shutil.rmtree(dir_list[i])
-            else:
-                 print(dir_list[i] + " is already deleted!")
-
+           remove_directory(dir_list[i])
+    else:
+        print("Type of "+dir_list+" mast be 'list'")
 def make_directory(directory_path):
     if not os.path.exists(directory_path):
         os.makedirs(directory_path)
@@ -38,13 +38,17 @@ def make_directories(dir_list):
     if type(dir_list) == list:
         for i in range(len(dir_list)):
             make_directory(dir_list[i])
-    else:print("Type of "+dir_list+" mast be 'list'")
+    else:
+        #TODO:format
+        print("Type of "+dir_list+" mast be 'list'")
 
 def png_to_jpg_converter(destination_path, filename):
-    if not os.path.exists(destination_path):  make_directory(destination_path)
+    make_directory(destination_path)
     if os.path.isfile(filename):
         im = Image.open(filename)
+        #TODO: file = 
         im.save(destination_path + filename.split("/")[-1].split(".png")[0] + ".jpg","jpeg")
-    else: print(filename + " not found!")
+    else:
+        print(filename + " not found!")
     
 
