9a91a9bdc72018154cd44b07c250bffbabba30dd
Taras <jackersson@users.noreply.github.com>
2017-11-05 12:22:54 +0200

    Update Parser.py

--- Parser.py
+++ Parser.py
@@ -5,6 +5,7 @@ import argparse
 import shutil
 import logging
 
+# TODO args: archive, destination
 def extract_zip(archive, dir_path):
     if os.path.exists(archive):
         with zipfile.ZipFile(archive, "r") as z:
@@ -12,25 +13,26 @@ def extract_zip(archive, dir_path):
     else:
         logging.error( u'Archive not found!')
         
-
+# TODO args: archive, destination
 def extract_tar(archive, dir_path):
     if os.path.exists(archive):
         with tarfile.open(archive) as tar:
             tar.extractall(path = dir_path)
     else:
         logging.error( u'Archive not found!')
-
+# TODO args: archive, destination
 def extract_archive(archive, dir_path): 
     filename, file_extension = os.path.splitext(archive)
     if file_extension == '.zip':
         extract_zip(archive, dir_path)
     elif file_extension == '.tar':
         extract_tar(archive, dir_path)
+    # TODO : else: not supported archive type
 
 class Parser(object):
     def __init__(self):
         pass
     def parse(self):
-        raise NotImplementedError("To be implemented")
+        raise NotImplementedError("Not implemented")
 
   
